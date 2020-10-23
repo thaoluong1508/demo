6ab7539fde66beab2a2c1c4e20ca4804bdb9bcef
ABC <ABC@DESKTOP-RPK6PAD>
2020-10-23 22:09:09 +0700

    commit for me

---
+++ QLNS/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using QLNS.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLNS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+         
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            MyModel obj = new MyModel();
+            obj.hoten = "Luong Thi Thao";
+            obj.tuoi = "21";
+            ViewBag.WelcomeString = "Welcome to the wild";
+            return View(obj);
+        }
+    }
+}
---
+++ QLNS/Models/MyModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QLNS.Models
+{
+    public class MyModel
+    {
+        public string hoten { get; set; }
+        public string tuoi { get; set; }
+    }
+}
